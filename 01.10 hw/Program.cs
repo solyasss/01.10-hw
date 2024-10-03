7ce868808ce213cfb1d65de6b2e78d99f1eac1d1
olya <olgastet01032005@gmail.com>
2024-10-03 13:26:15 +0300

    hw 03.10

--- 01.10 hw/Program.cs
+++ 01.10 hw/Program.cs
@@ -13,30 +13,68 @@ namespace hw
                 date_first.print();
 
                 Date second_date = new Date();
-                Console.WriteLine("\nBasic data");
+                Console.WriteLine("\nBasic date:");
                 second_date.print();
 
                 second_date.set_day(10);
                 second_date.set_month(12);
                 second_date.set_year(2024);
-                Console.WriteLine("\nNew data:");
+                Console.WriteLine("\nNew date:");
                 second_date.print();
 
-                int between= date_first.between_days(second_date);
-                Console.WriteLine($"\nAmount of days between dates");
+                // Using the overloaded - operator 
+                int between = second_date - date_first;
+                Console.WriteLine($"\nNumber of days between dates: {between}");
 
-                int add_days=10;
-                Console.WriteLine($"\nAdd {add_days} days to first date");
-                date_first.add(add_days);
+                // Using the overloaded + operator 
+                int add_days = 10;
+                Console.WriteLine($"\nAdding {add_days} days to the first date");
+                Date new_date = date_first + add_days;
+                new_date.print();
+
+                // Using the overloaded ++ operator 
+                Console.WriteLine("\nIncrementing the first date by one day");
+                ++date_first;
                 date_first.print();
+
+                // Using the overloaded -- operator 
+                Console.WriteLine("\nDecrementing the second date by one day");
+                --second_date;
+                second_date.print();
+
+                // Comparing dates using overloaded > and < operators
+                Console.WriteLine("\nComparing dates:");
+                if (date_first > second_date)
+                {
+                    Console.WriteLine("First date is later");
+                }
+                else if (date_first < second_date)
+                {
+                    Console.WriteLine("First date is earlier");
+                }
+                else
+                {
+                    Console.WriteLine("Both dates are the same.");
+                }
+
+                // Checking if dates are equal using overloaded == and != operators
+                Console.WriteLine("\nChecking if the dates are equal:");
+                if (date_first == second_date)
+                {
+                    Console.WriteLine("Dates are equal.");
+                }
+                else
+                {
+                    Console.WriteLine("Dates are not equal.");
+                }
             }
-            catch (ArgumentOutOfRangeException ex)
+            catch (ArgumentOutOfRangeException)
             {
-                Console.WriteLine($"Incorrect data");
+                Console.WriteLine("Incorrect data.");
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                Console.WriteLine($"Error:");
+                Console.WriteLine("Error");
             }
         }
     }
--- 01.10 hw/date.cs
+++ 01.10 hw/date.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace hw
-{ 
+{
     public class Date
     {
         private int day;
@@ -54,9 +54,9 @@ namespace hw
 
         private int days_in_month(int month, int year)
         {
-            int[] all_days= { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int[] all_days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
             if (month == 2 && leap_year(year))
-            return 29;
+                return 29;
             return all_days[month - 1];
         }
 
@@ -68,7 +68,6 @@ namespace hw
         public int between_days(Date date)
         {
             int difference = refill_into() - date.refill_into();
-
             if (difference < 0)
             {
                 return -difference;
@@ -114,6 +113,66 @@ namespace hw
         public void print()
         {
             Console.WriteLine($"{day:D2}.{month:D2}.{year}");
+        }
+
+        // Overloaded binary operator '-'
+        public static int operator -(Date date_1, Date date_2)
+        {
+            return date_1.refill_into() - date_2.refill_into();
+        }
+
+        // Overloaded operator '+'
+        public static Date operator +(Date date, int days)
+        {
+            Date N_Date = new Date(date.day, date.month, date.year);
+            N_Date.add(days);
+            return N_Date;
+        }
+
+        // Operator '++'
+        public static Date operator ++(Date date)
+        {
+            date.add(1);
+            return date;
+        }
+
+        // Operator '--'
+        public static Date operator --(Date date)
+        {
+            date.add(-1);
+            return date;
+        }
+
+        // Operator '>'
+        public static bool operator >(Date date_1, Date date_2)
+        {
+            return date_1.refill_into() > date_2.refill_into();
+        }
+
+        // Operator '<'
+        public static bool operator <(Date date_1, Date date_2)
+        {
+            return date_1.refill_into() < date_2.refill_into();
+        }
+
+        // Operator '=='
+        public static bool operator ==(Date date_1, Date date_2)
+        {
+            if ((object)date_1 == null && (object)date_2 == null)
+            {
+                return true;
+            }
+            if ((object)date_1 == null || (object)date_2 == null)
+            {
+                return false;
+            }
+            return date_1.day == date_2.day && date_1.month == date_2.month && date_1.year == date_2.year;
+        }
+
+        // Operator '!='                                                    
+        public static bool operator !=(Date date_1, Date date_2)
+        {
+            return !(date_1 == date_2);
         }
     }
 }
